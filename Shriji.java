7427e533c1a0e19c7cae1d7337b19ea120323a08
aryankumar <70devirkt@gmail.com>
2026-06-30 12:56:49 +0530

    added polymorphism

--- Shriji.java
+++ Shriji.java
@@ -1,28 +1,20 @@
-public class Shriji {
-    public static void main(String[] args)
-    {
-        Student s=new Student();
-        System.out.println(s.name);
-        // Output is :Person
-        // as abhi tak sirf name ka varibale sirf person mein hai toh ye print karega Person 
-        // ab maanlo ki maine name naam ka vriable bana liya hai ab Student mein toh 
-        // output kya aayega? 
-        System.out.println(s.name);
-        // Output is :Student
-
-        // lekin maanlo ye toh problem ho gayi hai maanlo mujhe abhi bhi use karna hai Person 
-        // wala name toh hum kaise karenge?
-
-        // ab yaha pe aata hai super keyword:
-
-
-    }
-}
-class Person{
-    String name="Person";
-}
-class Student extends Person{
-    String name="Student";
+// public class Shriji {
+//     public static void main(String[] args)
+//     {
+//         Car c=new ElectricCar();
     
-}
+//     }
+// }
+// class Car{
+//     private void fnc()
+//     {
+//         System.out.println("class");
+//     }
+// }
 
+// class ElectricCar extends Car{
+//     private void fnc()
+//     {
+//         System.out.println("subclass");
+//     }
+// }
